c15a3a9ec3ba52a06fa5573efc47d0efffe3c0c8
Matthew Leibowitz <mattleibow@live.com>
2018-04-18 16:59:13 +0200

    Added some "UI test"

---
+++ SecureStorageTest.UITest/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace SecureStorageTest.UITest
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp
+                    .Android
+                    .ApkFile(@"..\..\..\SecureStorageTest\bin\Release\SecureStorageTest.SecureStorageTest.apk")
+                    .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
